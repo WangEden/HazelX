3fcadec4bd10fdd90cc88ade921cf42fb67f3d42
WangEden <68456937+WangEden@users.noreply.github.com>
2026-02-16 19:49:48 +0800

    premake构建系统

---
+++ premake5.lua
@@ -0,0 +1,115 @@
+-- premake5.lua
+
+workspace "HazelX"
+    architecture "x64"
+    startproject "Sandbox"
+
+    configurations
+    {
+        "Debug", -- 调试版本，包含日志和调试信息
+        "Release", -- 发布版本，包含日志
+        "Dist" -- Distribution 完全没有日志的发布版本
+    }
+
+    filter "system:windows"
+        buildoptions { "/utf-8" }
+
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "HazelX"
+    location "HazelX"
+    kind "SharedLib" -- 动态链接库
+    language "C++"
+
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files
+    {
+        "%{prj.name}/src/**.h",
+        "%{prj.name}/src/**.cpp"
+    }
+
+    includedirs
+    {
+        "%{prj.name}/vendor/spdlog/include"
+    }
+
+    filter "system:windows"
+        cppdialect "C++17"
+        staticruntime "On" -- 静态链接运行时库
+        systemversion "latest"
+
+        defines
+        {
+            "HZ_PLATFORM_WINDOWS",
+            "HZ_BUILD_DLL",
+            "_WINDLL"
+        }
+
+        postbuildcommands
+        {
+            ("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+        }
+    
+    filter "configurations:Debug"
+        defines "HZ_DEBUG"
+        symbols "On" -- 开启调试符号
+
+    filter "configurations:Release"
+        defines "HZ_RELEASE"
+        optimize "On" -- 开启优化
+
+    filter "configurations:Dist"
+        defines "HZ_DIST"
+        optimize "On" -- 开启优化
+
+project "Sandbox"
+    location "Sandbox"
+    kind "ConsoleApp" -- 控制台应用程序
+    language "C++"
+
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+    objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files
+    {
+        "%{prj.name}/src/**.h",
+        "%{prj.name}/src/**.cpp"
+    }
+
+    includedirs
+    {
+        "HazelX/vendor/spdlog/include",
+        "HazelX/src"
+    }
+
+    links
+    {
+        "HazelX"
+    }
+
+    filter "system:windows"
+        cppdialect "C++17"
+        staticruntime "On" -- 静态链接运行时库
+        systemversion "latest"
+
+        defines
+        {
+            "HZ_PLATFORM_WINDOWS"
+        }
+
+    filter "configurations:Debug"
+        defines "HZ_DEBUG"
+        symbols "On" -- 开启调试符号
+
+    filter "configurations:Release"
+        defines "HZ_RELEASE"    
+        optimize "On" -- 开启优化
+
+    filter "configurations:Dist"
+        defines "HZ_DIST"
+        optimize "On" -- 开启优化
+
+    
