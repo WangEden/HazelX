7c04b682c6e0706648ad5db21ee4a50f085dfc8f
WangEden <68456937+WangEden@users.noreply.github.com>
2026-02-25 19:52:42 +0800

    add Basical ImGui Event

--- HazelX/src/Platform/Windows/WindowsWindow.cpp
+++ HazelX/src/Platform/Windows/WindowsWindow.cpp
@@ -98,6 +98,14 @@ namespace Hazel {
 			}
 		});
 
+		glfwSetCharCallback(m_Window, [](GLFWwindow* window, unsigned int keycode) {
+			WindowData& data = *(WindowData*)glfwGetWindowUserPointer(window);
+
+			KeyTypedEvent event(keycode);
+			data.EventCallback(event);
+		});
+
+
 		// 鼠标事件回调
 		glfwSetMouseButtonCallback(m_Window, [](GLFWwindow* window, int button, int action, int mods) {
 			WindowData& data = *(WindowData*)glfwGetWindowUserPointer(window);
