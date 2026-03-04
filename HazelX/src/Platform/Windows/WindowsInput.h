d99481ebb640c92f1a54ffe903a5da0d3f64af75
WangEden <68456937+WangEden@users.noreply.github.com>
2026-03-04 14:44:43 +0800

    Input Polling

---
+++ HazelX/src/Platform/Windows/WindowsInput.h
@@ -0,0 +1,18 @@
+#pragma once
+
+#include "Hazel/Input.h"
+
+namespace Hazel {
+
+	// 不需要导出到dll
+	class WindowsInput : public Input
+	{
+	protected:
+		virtual bool IsKeyPressedImpl(int keycode) override;
+
+		virtual bool IsMouseButtonPressedImpl(int button) override;
+		virtual std::pair<float, float> GetMousePositionImpl() override;
+		virtual float GetMouseXImpl() override;
+		virtual float GetMouseYImpl() override;
+	};
+}
