86df4d825e222f7c9cfc2c0d292eb5f6d7559d39
WangEden <68456937+WangEden@users.noreply.github.com>
2026-03-04 16:33:53 +0800

    add KeyCode and MouseCode

--- HazelX/src/Hazel/ImGui/ImGuiLayer.cpp
+++ HazelX/src/Hazel/ImGui/ImGuiLayer.cpp
@@ -64,7 +64,7 @@ namespace Hazel {
 	
 	void ImGuiLayer::OnEvent(Event& event)
 	{
-		HZ_CORE_TRACE("ImGuiLayer::Event {0}", event);
+		//HZ_CORE_TRACE("ImGuiLayer::Event {0}", event);
 		EventDispatcher dispatcher(event);
 		dispatcher.Dispatch<MouseButtonPressedEvent>(HZ_BIND_EVENT_FN(ImGuiLayer::OnMouseButtonPressedEvent));
 		dispatcher.Dispatch<MouseButtonReleasedEvent>(HZ_BIND_EVENT_FN(ImGuiLayer::OnMouseButtonReleasedEvent));
