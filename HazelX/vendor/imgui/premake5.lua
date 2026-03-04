a59d5bb60f622a90fe208b95da6ed9f7a22050d2
WangEden <68456937+WangEden@users.noreply.github.com>
2026-03-04 11:55:15 +0800

    change implements

---
+++ HazelX/vendor/imgui/premake5.lua
@@ -0,0 +1,42 @@
+project "ImGui"
+    kind "StaticLib"
+    language "C++"
+    staticruntime "on"
+
+    targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+    files
+    {
+        "imconfig.h",
+        "imgui.h",
+        "imgui.cpp",
+        "imgui_draw.cpp",
+        "imgui_internal.h",
+        "imgui_widgets.cpp",
+        "imgui_tables.cpp",
+        "imstb_rectpack.h",
+        "imstb_textedit.h",
+        "imstb_truetype.h",
+        "imgui_demo.cpp",
+        -- "imgui_impl_glfw.cpp",
+        -- "imgui_impl_glfw.h",
+        -- "imgui_impl_opengl3.cpp",
+        -- "imgui_impl_opengl3.h",
+    }
+
+    -- IncludeDir = {}
+    -- IncludeDir["GLFW"] = "HazelX/vendor/GLFW/include"
+    -- include "HazelX/vendor/GLFW"
+
+    -- includedirs
+    -- {
+    --     "%{IncludeDir.GLFW}",
+    -- }
+
+    filter "system:windows"
+        systemversion "latest"
+        cppdialect "C++17"
+
+    filter { "system:windows", "configurations:Release" }
+        buildoptions "/MT"
