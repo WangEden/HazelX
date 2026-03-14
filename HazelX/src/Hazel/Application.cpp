ea372dacdcf994e1ecef72271faa402f3800c328
WangEden <wangeden_@outlook.com>
2026-03-14 17:25:55 +0800

    OpenGL Shader

--- HazelX/src/Hazel/Application.cpp
+++ HazelX/src/Hazel/Application.cpp
@@ -50,6 +50,35 @@ namespace Hazel {
 
 		unsigned int indices[3] = { 0, 1, 2 }; // 定义索引数据，坐下开始逆时针绘制三角形
 		glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW); // 将索引数据传输到GPU
+
+		std::string vertexSrc = R"(
+			#version 330 core
+			
+			layout(location = 0) in vec3 a_Position;
+
+			out vec3 v_Position; // 传递给片段着色器的变量
+		
+			void main()
+			{
+				v_Position = a_Position;
+				gl_Position = vec4(a_Position, 1.0);
+			}
+		)";
+
+		std::string fragmentSrc = R"(
+			#version 330 core
+			
+			layout(location = 0) out vec4 color;
+
+			in vec3 v_Position;
+			
+			void main()
+			{
+				color = vec4(v_Position * 0.5 + 0.5, 1.0);
+			}
+		)";
+
+		m_Shader.reset(new Shader(vertexSrc, fragmentSrc));
 	}
 
 	Application::~Application()
@@ -88,6 +117,7 @@ namespace Hazel {
 			glClearColor(0.1f, 0.1f, 0.1f, 1);
 			glClear(GL_COLOR_BUFFER_BIT);
 
+			m_Shader->Bind();
 			glBindVertexArray(m_VertexArray);
 			glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_INT, nullptr); // 绘制三角形，参数1表示绘制模式，参数2表示索引数量，参数3表示索引数据类型，参数4表示索引数据在缓冲区中的偏移量
 			
